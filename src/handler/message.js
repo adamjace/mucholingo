4a72e6c276e3a351b11cb31691fcc5e4d2959fa9
Adam Goldin <adam.goldin@simplehq.com.au>
2016-08-02 21:54:44 +1000

    google translate integration

--- src/handler/message.js
+++ src/handler/message.js
@@ -1,11 +1,9 @@
-const Translator = require('../lib/translator')
+const t = require('../lib/translator')
 const Logger = require('../lib/logger')
-const languages = require('../lib/language')
+const languages = require('../lib/lang')
 const mixpanel = require('../lib/mixpanel')
 const db = require('../db/redis')
 const _ = require('lodash')
-
-const t = new Translator()
 
 class MessageHandler {
 
@@ -13,18 +11,18 @@ class MessageHandler {
     const { sender, message } = payload
     bot.getProfile(sender.id, (err, profile) => {
       if (err) throw err
-      if (_.includes(message.text, '/getstarted')) return MessageHandler._handleGetStarted(sender, profile, reply)
-      if (_.includes(message.text, '/change')) return MessageHandler._handleChange(sender, reply)
+      if (_.includes(message.text, '/getstarted')) return MessageHandler.handleGetStarted(sender, profile, reply)
+      if (_.includes(message.text, '/change')) return MessageHandler.handleChange(sender, reply)
       // check if we have a context for translation
-      db.getAsync(sender.id).then((value) => {
-        if (value === null) return MessageHandler._handleNoContext(sender, message, reply)
+      db.getAsync(sender.id).then((context) => {
+        if (context === null) return MessageHandler.handleNoContext(sender, message, reply)
         // we made it! translate the message
-        MessageHandler._handleTranslation(sender, message, reply) 
+        MessageHandler.handleTranslation(context, sender, message, reply) 
       })
     })
   }
 
-  static _handleGetStarted(sender, profile, reply) {
+  static handleGetStarted(sender, profile, reply) {
     reply({
       text: `Hello, hola and bonjour! Let's get started. What language you would like me to translate for you? For example, type "english to spanish" or "german to french"`
     })
@@ -32,7 +30,7 @@ class MessageHandler {
     mixpanel.track('I click to get started', sender)
   }
 
-  static _handleChange(sender, reply) {
+  static handleChange(sender, reply) {
     db.delAsync(sender.id).then((err, resp) => {
       reply({
         text: `Sure thing! What language you would like me to translate for you now? For example, type "english to spanish" or "german to french"`
@@ -41,52 +39,54 @@ class MessageHandler {
     })
   }
 
-  static _handleNoContext(sender, message, reply) {
-    const context = MessageHandler._getContext(message.text)
+  static handleNoContext(sender, message, reply) {
+    const context = MessageHandler.getContext(message.text)
     if (!context.has) {
       return reply({
         text: `I didn't quite catch that. Tell me what language you would like me to translate. For example, type "english to spanish" or "german to french"`
       })
       mixpanel.track('I incorrectly set context', sender, message)
     }
-    return MessageHandler._handleContext(context, sender, message, reply)
+    return MessageHandler.handleContext(context, sender, message, reply)
   }
 
-  static _handleContext(context, sender, message, reply) {
+  static handleContext(context, sender, message, reply) {
     // we have context, store it and reply
-    const contextValue = `${context.matches[0]}-${context.matches[1]}`
+    const contextValue = `${context.matches[0].code}:${context.matches[1].code}`
     db.setAsync(sender.id, contextValue).then((err, resp) => {
       return reply({
-        text: `Got it! ${_.capitalize(context.matches[0])} to ${_.capitalize(context.matches[1])}. Now go ahead and tell me what to translate. Type "/change" at anytime to switch languages`
+        text: `Got it! ${_.capitalize(context.matches[0].name)} to ${_.capitalize(context.matches[1].name)}. Now go ahead and tell me what to translate. Type "/change" at anytime to switch languages`
       })
       mixpanel.track('I set context', sender, message)
     })
   }
 
-  static _handleTranslation(sender, message, reply) {
-    reply({ text: MessageHandler._translateMessage(message.text) }, (err) => {
-      if (err) throw err
-      mixpanel.track('I send a message to be translated', sender)
-    })
+  static handleTranslation(context, sender, message, reply) {
+    t.translate(message.text, context).
+      then((result) => {
+        reply({ text: result }, (err) => {
+          if (err) throw err
+          mixpanel.track('I send a message to be translated', sender)
+        })
+      }).
+      catch((err) => {
+        reply({ text: `Oh no, something has gone wrong. Please try that again` })
+      })
   }
 
-  static _getContext(message) {
+  static getContext(message) {
     let matches = []
     const words = message.split(' ')
     words.forEach((word) => {
-      if (_.includes(languages, word)) {
-        matches.push(word)
+      languages.filter(item => item.name === _.lowerCase(word)).map((lang) => {
         if (matches.length === 2) return
-      }
+        matches.push(lang)
+      })
     })
     return {
       has: matches.length === 2,
       matches: matches
     }
-  }
-
-  static _translateMessage(message, context) {
-    return t.translate(message, context)
   }
 }
 
--- src/lib/translator.js
+++ src/lib/translator.js
@@ -1,9 +1,20 @@
+const config = require('../config')
+const googleTranslate = require('google-translate')(config.google_key)
+const Promise = require('bluebird')
+
 class Translator {
   
-  translate(message, context) {
-    return 'This is a translated message...'
+  static translate(message, context) {
+    const arr = context.split(':')
+    const source = arr[0]
+    const target = arr[1]
+    return new Promise((resolve, reject) => {
+      googleTranslate.translate(message, source, target, (err, translation) => {
+        if (err) return reject(err)
+        resolve(translation.translatedText)
+      })
+    })
   }
-
 }
 
 module.exports = Translator
