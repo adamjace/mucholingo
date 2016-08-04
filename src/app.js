86e5c7ab61260b222bafd8e906be56deab42055d
adamjace <adamjace@gmail.com>
2016-08-05 05:21:34 +1000

    npm start scripts

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ const Logger = require('./lib/logger')
 const config = require('./config')
 const Routes = require('./routes')
 
+const env = process.env.NODE_ENV || 'development';
+
 // create the app
 let app = express()
 app.use(bodyParser.json())
@@ -22,10 +24,13 @@ Routes.makeRoutes(app)
 // start the server
 http.createServer(app).listen(`${config.port}`)
 
-// secure server
-const options = {
-  key: fs.readFileSync('ssl/key.pem'),
-  cert: fs.readFileSync('ssl/cert.pem')
+// create https
+if (env === 'production') {
+  const options = {
+    key: fs.readFileSync('ssl/key.pem'),
+    cert: fs.readFileSync('ssl/cert.pem')
+  }
+  https.createServer(options, app).listen(443)
 }
-https.createServer(options, app).listen(443)
+
 Logger.log(`Lingo Bot server running on port ${config.port}`)
